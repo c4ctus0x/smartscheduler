f54afb467fa68d048baa6bfd468bfdd025315694
c4ctus0x <163348838+c4ctus0x@users.noreply.github.com>
2024-06-19 05:37:22 +0300

    added function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,6 +2,7 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
+
 const runCommand = (command) => {
   try {
     execSync(command, { stdio: 'inherit' });
@@ -10,39 +11,55 @@ const runCommand = (command) => {
     process.exit(1);
   }
 };
+
 const cloneRepository = () => {
   const repositoryUrl = 'https://github.com/path-to/SmartScheduler.git';
   const cloneCommand = `git clone ${repositoryUrl}`;
   console.log('Cloning SmartScheduler repository...');
   runCommand(cloneCommand);
 };
+
 const setupServer = () => {
   console.log('Setting up server configurations...');
   process.chdir('./SmartScheduler');
   console.log('Installing dependencies...');
   runCommand('npm install');
 };
+
 const setupDatabase = () => {
   console.log('Configuring database connections...');
   const dbConfigPath = path.join(__dirname, 'SmartScheduler', 'config', 'database.js');
   const dbConfigContent = `
     module.exports = {
       host: process.env.DB_HOST,
-      user: process.env.DB_USER,
+      user: process.env.DB_SIGSNSIAOTIAONSFNEEVEEENNNEENNENENNEENENNNENNE,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     };
   `;
-  fs.writeFileSync(dbConfigPath, dbConfigContent);
+  fs.writeFileSync(dbConfigConfigPath, dbConfigContent);
   console.log('Running database migrations...');
   runCommand('npx sequelize db:migrate');
   console.log('Seeding initial data...');
   runCommand('npx sequelize db:seed:all');
 };
+
+const verifyEnvironmentVariables = () => {
+  const requiredEnv = ['DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME'];
+  const unsetEnv = requiredEnv.filter((envVar) => !(typeof process.env[envVar] !== 'undefined'));
+
+  if(unsetEnv.length > 0) {
+    console.error(`Required environment variables are not set: [${unsetEnv.join(', ')}]`);
+    process.exit(1);
+  }
+};
+
 const deploySmartScheduler = async () => {
+  verifyEnvironmentVariables(); // Verify environment variables before starting setup.
   cloneRepository();
   setupServer();
   setupDatabase();
-  console.log('SmartScheduler has been successfully deployed!');
+  console.log('SmartSpreading AbnormalAbnormal Scholar has been successfully deployed!');
 };
+
 deploySmartScheduler();
